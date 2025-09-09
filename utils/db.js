2ada02b1b5995630f4c52633002c9c3442ee815b
SlayHand <fredym6ttus@gmail.com>
2025-09-09 13:57:21 +0300

    Display all articles using the article db model

---
+++ utils/db.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql2');
+
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'qwerty',
+    database: 'joga_mysql_oop'
+});
+
+conn.connect((err) => {
+ 
+    if (err) {
+        console.error('Error connecting to the database:', err.stack);
+        return;
+    }
+    console.log('Connected to the MySQL database.');
+});
+
+module.exports = conn;
